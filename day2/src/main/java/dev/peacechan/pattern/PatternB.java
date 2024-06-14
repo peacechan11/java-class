866db1881d268cd4a527ae06f217c4a3e63f13cc
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-06-14 23:49:13 +0630

    pattern with for loop

---
+++ day2/src/main/java/dev/peacechan/pattern/PatternB.java
@@ -0,0 +1,15 @@
+package dev.peacechan.pattern;
+
+public class PatternB {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
